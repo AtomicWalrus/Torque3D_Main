c60f34e664746d3fd67b2e079f0ef631b0945ddf
AtomicWalrus <headlesschickens.inc@gmail.com>
2023-04-04 01:52:53 -0600

    Shaders for reverse depth

--- Templates/BaseGame/game/core/rendering/shaders/water/gl/waterV.glsl
+++ Templates/BaseGame/game/core/rendering/shaders/water/gl/waterV.glsl
@@ -173,7 +173,7 @@ void main()
    // }
    
    // Save world space camera dist/depth of the outgoing pixel
-   OUT_rippleTexCoord2.z = OUT_hpos.w; // [ZREV] for reversed depth   
+   OUT_rippleTexCoord2.z = OUT_hpos.w; // Reversed depth
 
    // Convert to reflection texture space   
    OUT_posPostWave = tMul( texGen, OUT_posPostWave );
